601554c444506f1481842196fc5cb9f2a7d1acec
UserTundra <haizbq@gmail.com>
2018-05-14 19:16:08 +0400

    initial commit

---
+++ Wallet/WalletClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet
+{
+    public class WalletClass
+    {
+        
+        private Dictionary<string, int> currency = new Dictionary<string, int>();
+        int currentMoney;       
+
+        public WalletClass()
+        {
+            
+        }
+
+        public void add(string currency, int money)
+        {
+            
+        }
+
+        public void remove(string currency, int money)
+        {
+            
+        }
+    }
+}
