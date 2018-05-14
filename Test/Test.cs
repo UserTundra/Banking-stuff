84ae99c4814fdb78be4cc03b1d4771ede2edda94
UserTundra <haizbq@gmail.com>
2018-05-14 21:57:55 +0400

    +add()

--- Test/Test.cs
+++ Test/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wallet;
 
 namespace Test
 {
@@ -9,7 +10,32 @@ namespace Test
         [TestMethod]
         public void CreateTest()
         {
-            
+            Money money = new Money();
+            Assert.AreEqual(0, money.getKeys());
+
         }
+
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Money money = new Money();
+            money.add("rub", 100);
+            money.add("euro", 300);
+            Assert.AreEqual(2, money.getKeys());
+            Assert.AreEqual(100, money.getValues("rub"));
+            Assert.AreEqual(300, money.getValues("euro"));
+        }
+
+        [TestMethod]
+        public void AddTest2()
+        {
+            Money money = new Money();
+            money.add("rub", 100);
+            money.add("rub", 300);
+            Assert.AreEqual(1, money.getKeys());
+            Assert.AreEqual(400, money.getValues("rub"));
+        }
+
     }
 }
---
+++ Wallet/Money.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet
+{
+    public class Money
+    {
+        
+        private Dictionary<string, int> account = new Dictionary<string, int>();
+        int currentMoney;   
+        
+
+        public Money()
+        {
+            
+        }
+
+        public int getKeys()
+        {
+            return account.Keys.Count;
+        }
+
+        public int getValues(string key)
+        {
+            return account[key];
+        }
+
+        public void add(string currency, int money)
+        {
+            if (account.Keys.Contains(currency))
+            {
+                account[currency] += money;
+            }
+            else
+            {
+                account.Add(currency, money);
+            }
+        }
+
+        public void remove(string currency, int money)
+        {
+            
+        }
+    }
+}
